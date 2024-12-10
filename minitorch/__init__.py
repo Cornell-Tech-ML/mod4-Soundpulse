13a3dad730efd8c5ced824ef3e1bc2cfde5206c1
Toby Io <toby.io@outlook.com>
2024-12-10 11:48:32 -0500

    Fix pre-commit again

--- minitorch/__init__.py
+++ minitorch/__init__.py
@@ -1,3 +1,5 @@
+"""Module 4 of Minitorch Module"""
+
 from .testing import MathTest, MathTestVariable  # type: ignore # noqa: F401,F403
 from .datasets import *  # noqa: F401,F403
 from .optim import *  # noqa: F401,F403
